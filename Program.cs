9b5e5a1c62f7fdbb02201fa7297d148de2551701
nhank <nhank@HELLO23241>
2025-09-09 20:44:24 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+bakery: tiệm bánh
+news: tin tức
+agent: điệp viên
+newsagent: nhà đài
+shopkeeper: người bán hàng
+cash: tiền mặt
+bottle: chai -> bottle of water: chai nước
+contain = tích trữ
+store: cửa hàng, đồ tích trữ
